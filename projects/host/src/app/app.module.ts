235e8b6723ffec712afbb2cc3e16e6d71a448184
JP-Jagadesan <jagadesanp123@gmail.com>
2023-05-25 21:52:43 +0530

    inital code setup

---
+++ projects/host/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TestModule } from 'test/first';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TestModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ projects/test/first/index.ts
@@ -0,0 +1,4 @@
+
+export * from './src/test.service';
+export * from './src/test.component';
+export * from './src/test.module';
